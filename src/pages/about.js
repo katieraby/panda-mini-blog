6782229047c8fe8e4362b55646edac67717321ee
Katie <50029859+katieraby@users.noreply.github.com>
2020-06-28 21:45:31 +0100

    Added graphQL query in page component, and staticQuery graphQL query in layout component

---
+++ src/pages/about.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default function About({ data }) {
+  return (
+    <Layout>
+      <h1>About {data.site.siteMetadata.title}</h1>
+      <p>
+        We're the only site running on your computer dedicated to showing the
+        best photos and videos of pandas eating lots of food.
+      </p>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+// Page queries live outside of the component definition — by convention at the end of a page component file — and are only available on page components.
